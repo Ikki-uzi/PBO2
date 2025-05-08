93f6a3939eb651f5980efa0a7d6405defa1c0860
Ikki-uzi <rizky.ddn@gmail.com>
2025-05-08 11:59:54 +0700

    UTSPBO2

---
+++ UTS_PBO2/src/main/java/com/mycompany/uts_pbo2/UTS_PBO2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uts_pbo2;
+
+/**
+ *
+ * @author rizki
+ */
+public class UTS_PBO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
